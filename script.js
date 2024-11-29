14e5c487efcd0238b01e1ed3c970334d964f4ecb
AbdulazizJabri <nommon1230789@gmail.com>
2024-11-29 23:14:08 +0400

    .

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-// Display information for each hotspot
-document.querySelectorAll('.hotspot').forEach((hotspot) => {
-    hotspot.addEventListener('click', () => {
-        alert(hotspot.getAttribute('data-info'));
-    });
+// Quiz Form Submission
+document.getElementById('quiz-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let score = 0;
+    const answer1 = document.getElementById('question1').value;
+    const answer2 = document.getElementById('question2').value;
+
+    // Check answers
+    if (answer1 === "2") score++;
+    if (answer2 === "3") score++;
+
+    // Display result
+    let resultMessage = "You scored " + score + " out of 2!";
+    if (score === 2) {
+        resultMessage += " Excellent knowledge!";
+    } else if (score === 1) {
+        resultMessage += " Nice try! You're getting there.";
+    } else {
+        resultMessage += " Keep learning!";
+    }
+
+    document.getElementById('quiz-result').innerHTML = resultMessage;
 });
